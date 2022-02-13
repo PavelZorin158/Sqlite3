19f767649bdfe01c08e3503fca652098d1c72e7a
PavelZorin158 <pasharick@gmail.com>
2022-02-13 18:02:56 +0300

    First commit

---
+++ ex1.py
@@ -0,0 +1,11 @@
+import sqlite3 as sq
+
+with sq.connect("saper.db") as con:
+    cur = con.cursor()
+
+    cur.execute("""CREATE TABLE IF NOT EXISTS users(
+    name TEXT,
+    sex INTEGER,
+    old INTEGER,
+    score INTEGER
+    )""")
